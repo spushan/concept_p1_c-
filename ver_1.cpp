8acd76e1465647f5a170b7e9b8ec6400285cad52
spushan <brent_002@yahoo.com>
2020-06-08 20:26:34 -0700

    init commit

---
+++ ver_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void NewMatrix(int row, int col) {
+    float num;
+    float (*a)[100] = new float[100][100];
+    for(int i=0, i<row; i++){
+        for(int j=0; j<col; j++){
+            cout<<"Enter value of m["<<i<<"]"<<"["<<j<<"]: ";
+            cin>>num;
+            a[i][j] = num;
+        }
+    }
+    return a;
+}
+
+void main() {
+
+}
