ea8176269a743e0597db918809e024405151936d
spushan <brent_002@yahoo.com>
2020-06-09 15:30:09 -0700

    newmat changed

--- ver_1.cpp
+++ ver_1.cpp
@@ -3,12 +3,13 @@
 
 using namespace std;
 
-void NewMatrix(int row, int col, float*matrix) {
-
+void NewMatrix(int row, int col, float matrix[][100]) {
+    float num;
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
             cout<<"Enter value of m["<<i<<"]"<<"["<<j<<"]: ";
-            cin>matrix[i][j];
+            cin>>num;
+            matrix[i][j] = num;
         }
     }
 }
@@ -37,6 +38,10 @@ int main() {
     float mat1[100][100];
     float mat2[100][100];
     float result[100][100];
+
+    NewMatrix(5,5,mat1);
+    NewMatrix(5,5,mat2);
+
     
     return 0;
 }
