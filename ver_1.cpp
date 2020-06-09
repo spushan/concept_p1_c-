714a0c5eb2d26e3c87b77dc67b686bea41c64e23
spushan <brent_002@yahoo.com>
2020-06-09 15:12:20 -0700

    changes

--- ver_1.cpp
+++ ver_1.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
-void *NewMatrix(int row, int col) {
-    float num;
-    float (*a)[100] = new float[100][100];
+void NewMatrix(int row, int col, float*matrix) {
+
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
             cout<<"Enter value of m["<<i<<"]"<<"["<<j<<"]: ";
-            cin>>num;
-            a[i][j] = num;
+            cin>matrix[i][j];
         }
     }
-    return a;
 }
 
 
@@ -24,19 +23,20 @@ void PrintMatrix(float matrix[100][100], int row, int col) {
     cout<<endl;
 }
 
-void *AddMatrix(float mat1[100][100], float mat2[100][100], float result[100][100], int row, int col){
-
+void *AddMatrix(float mat1[100][100], float mat2[100][100], float* result[100][100], int row, int col){
 
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             result[i][j] = mat1[i][j] + mat2[i][j];
         }
     }
-
-
 }
 
-void main() {
-
-
+int main() {
+    
+    float mat1[100][100];
+    float mat2[100][100];
+    float result[100][100];
+    
+    return 0;
 }
