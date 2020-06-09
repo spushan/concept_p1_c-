5a651cb54a3664663568a17df92ba21b8aa91b2a
spushan <brent_002@yahoo.com>
2020-06-09 13:58:20 -0700

    print

--- ver_1.cpp
+++ ver_1.cpp
@@ -14,6 +14,29 @@ void *NewMatrix(int row, int col) {
     return a;
 }
 
+
+void PrintMatrix(float matrix[100][100], int row, int col) {
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<matrix[i][j]<<"  ";
+        }
+    }
+    cout<<endl;
+}
+
+void *AddMatrix(float mat1[100][100], float mat2[100][100], float result[100][100], int row, int col){
+
+
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            result[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+
+}
+
 void main() {
+
 
 }
